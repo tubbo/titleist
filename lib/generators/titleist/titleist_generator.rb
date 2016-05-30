60ed41cff05e0d118fe01dd8e3b4d977f8c3ab03
Tom Scott <tscott@weblinc.com>
2016-05-30 17:06:25 -0400

    Update generator to use templates

--- lib/generators/titleist/titleist_generator.rb
+++ lib/generators/titleist/titleist_generator.rb
@@ -3,17 +3,14 @@ class TitleistGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_locale
-    copy_file(
-      'config/locales/en.yml',
-      'config/locales/titleist.en.yml'
-    )
+    template 'locale.yml.erb', 'config/locales/titleist.en.yml'
   end
 
-  def update_application_layout
+  def update_default_application_layout
     gsub_file(
       'app/views/layouts/application.html.erb',
-      %r{<title>(.*)</title>},
-      '<%= title_tag %>'
+      %r{\s\s\s\s<title>(.*)</title>},
+      '    <%= title_tag %>'
     )
   end
 end
