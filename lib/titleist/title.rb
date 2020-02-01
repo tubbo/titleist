402699e7c1eec1cfe434675e411396c8d3933171
Tom Scott <tscott@weblinc.com>
2020-02-01 14:39:37 -0500

    Add method signatures and typechecking to Title

--- lib/titleist/title.rb
+++ lib/titleist/title.rb
@@ -4,6 +4,8 @@ module Titleist
   # Object that turns passed-in scope details into a String of title
   # text derived from the +I18n+ locale configuration.
   class Title
+    extend T::Sig
+
     # Default title format that can be overridden by changing the i18n
     # key `titelist.format`.
     FORMAT = '%{page} - %{app}'.freeze
@@ -39,6 +41,7 @@ module Titleist
     # Global application title.
     #
     # @return [String]
+    sig { returns(String) }
     def app
       @app ||= I18n.t :title, @params.reverse_merge(
         scope: %i[layouts application],
@@ -49,6 +52,7 @@ module Titleist
     # Page title from the current scope.
     #
     # @return [String]
+    sig { returns(String) }
     def page
       @page ||= I18n.t :title, @params.reverse_merge(
         scope: [*controller_scope, @action],
@@ -64,22 +68,26 @@ module Titleist
     # Format the full page title.
     #
     # @return [String]
+    sig { returns(String) }
     def to_s
       I18n.t :format, scope: :titleist, default: FORMAT, app: app, page: page
     end
 
     private
 
+    sig { returns(String) }
     def default_app_title
       Rails.application.class.name&.deconstantize&.titleize.to_s
     end
 
+    sig { returns(String) }
     def default_page_title
       return @controller.titleize if @action == 'index'
 
       [action_title, resource_title].join(' ')
     end
 
+    sig { returns(String) }
     def action_title
       case @action
       when 'show'
@@ -91,10 +99,12 @@ module Titleist
       end
     end
 
+    sig { returns(String) }
     def resource_title
       @controller.singularize.titleize
     end
 
+    sig { returns(Array) }
     def controller_scope
       @controller.split('/')
     end
