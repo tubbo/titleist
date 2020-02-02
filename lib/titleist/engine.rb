ec55d2870b66e58113aa80fe2d5bdaefc14662fb
Tom Scott <tscott@weblinc.com>
2020-02-02 15:22:44 -0500

    Fix load hook for Titleist

--- lib/titleist/engine.rb
+++ lib/titleist/engine.rb
@@ -3,7 +3,7 @@ module Titleist
   # Extensions into the Rails framework that mixin the controller and
   # helper modules when ActionController and ActionView are loaded.
   class Engine < Rails::Engine
-    ActiveSupport.on_load :action_controller do
+    ActiveSupport.on_load :action_controller_base do
       include Titleist::Controller
     end
 
